45e5081a99a19b5c66338c4d1d291b211c9f5fd3
Andrew Lupton <aflupton@gmail.com>
2018-04-27 09:40:04 -0700

    add initial files and dotnet restore

---
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounterApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace WordCounterApp.TestTools
+{
+  [TestClass]
+  public class WordCounterAppTest
+}
